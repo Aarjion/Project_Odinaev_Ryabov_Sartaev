177b82d50cd07db3a0dfd39770eb9e4724cf638f
Max Ryabov <rockmaks_00@mail.ru>
2021-12-20 00:47:08 +0500

    back-end 2

--- wine/app/tests.py
+++ wine/app/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from .models import Wine
 
-# Create your tests here.
+
+def test():
+    wine = Wine.create(7, 0.27, 0.36, 20.7, 0.045, 45, 170, 1.001, 3, 0.45, 8.8)
+    return wine.find_quality()
+
+
+print(test())
