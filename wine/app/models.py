68480acceb925911faf2258be0028c622c470e46
Max Ryabov <rockmaks_00@mail.ru>
2021-12-19 21:56:37 +0500

    back-end 1

--- wine/app/models.py
+++ wine/app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Wine(models.Model):
+    fixed_acidity = models.FloatField('fixed acidity')
+    volatile_acidity = models.FloatField('volatile acidity')
+    citric_acid = models.FloatField('citric acid')
+    residual_sugar = models.FloatField('residual sugar')
+    chlorides = models.FloatField('chlorides')
+    free_sulfur = models.FloatField('free sulfur')
+    total_sulfur = models.FloatField('total sulfur')
+    density = models.FloatField('density')
+    pH = models.FloatField('pH')
+    sulphates = models.FloatField('sulphates')
+    alcohol = models.FloatField('alcohol')
+    quality = models.FloatField('quality')
