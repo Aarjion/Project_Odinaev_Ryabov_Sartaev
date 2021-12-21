2a6e08bf6c80ed627f2235e1b7a50d21fd8ca829
Max Ryabov <rockmaks_00@mail.ru>
2021-12-21 22:10:06 +0500

    back-end 3

--- wine/app/views.py
+++ wine/app/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from .models import Wine
 
 
-# Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+
+def result(request):
+    if 'wine_data' in request.GET:
+        wine_data = request.GET['wine_data']
+        wine = Wine.create(wine_data['fixed_acidity'], wine_data['volatile_acidity'], wine_data['citric_acid'],
+                           wine_data['residual_sugar'], wine_data['chlorides'], wine_data['free_sulfur'],
+                           wine_data['total_sulfur'], wine_data['density'], wine_data['pH'], wine_data['sulphates'],
+                           wine_data['alcohol'])
+        wine.quality = wine.find_quality()
+        return render(request, 'result.html', {'result': wine})
